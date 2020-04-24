4334949e34e2ad67e59113c046ea30f1f2d66109
masmelacarol <carolstefannym@gmail.com>
2020-04-24 15:50:44 -0500

    new: se agregan algunas imagenes que se utilizan en el header y en footer

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import reduxThunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
 
 //Reducers
-import reducers from './redux/reducers/index';
+import reducers from "./redux/reducers/index";
 //
-import App from './routes/App';
+import App from "./routes/App";
 
 const store = createStore(
-    reducers, //Todos los reducer
-    {}, //Estado inicial
-    applyMiddleware(reduxThunk)
-) 
+  reducers, //Todos los reducer
+  {}, //Estado inicial
+  applyMiddleware(reduxThunk)
+);
 
-ReactDOM.render( 
-    <Provider store={store}>
-        < App / >
-    </Provider>  , document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
