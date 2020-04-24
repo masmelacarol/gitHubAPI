4c953d7e06b6bfe889f7b50bc28868ec4cd444d3
masmelacarol <carolstefannym@gmail.com>
2020-04-23 22:23:58 -0500

    new: se crea la estuctura del proyecto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+
+//Reducers
+import reducers from './redux/reducers/index';
+//
+import App from './routes/App';
+
+const store = createStore(
+    reducers, //Todos los reducer
+    {}, //Estado inicial
+    applyMiddleware(reduxThunk)
+) 
+
+ReactDOM.render( 
+    <Provider store={store}>
+        < App / >
+    </Provider>  , document.getElementById('app'));
