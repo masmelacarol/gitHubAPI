de469b4c6e7bfe4f8e992510ba00c05873284685
masmelacarol <carolstefannym@gmail.com>
2020-06-09 10:53:15 -0500

    update: actualizando dependencias

---
+++ src/pages/UserGitPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import DataForm from '../components/DataForm';
+// import { connect } from "react-redux";
+// import {
+//   updateInputValue,
+//   dataUser,
+//   getAllRepos,
+//   validateInputs,
+// } from "../redux/actions/usersRepoActions";
+
+
+const UserGitPage = (props) => (
+  <React.Fragment>
+    <DataForm/>    
+  </React.Fragment>
+);
+
+export default UserGitPage;
